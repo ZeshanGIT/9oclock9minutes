392c1f17f04e4c024e093393a2052b3df19ef0a6
Seshan <zeshan.nandan@gmail.com>
2020-04-03 13:39:54 +0530

    done i guess

--- app/src/main/java/edu/sastra/a9oclock9minutes/AlarmService.java
+++ app/src/main/java/edu/sastra/a9oclock9minutes/AlarmService.java
@@ -1,52 +1,51 @@
 package edu.sastra.a9oclock9minutes;
 
-import android.app.NotificationManager;
-import android.app.Service;
-import android.app.job.JobInfo;
-import android.app.job.JobParameters;
-import android.app.job.JobScheduler;
-import android.app.job.JobService;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.os.Build;
 import android.os.Handler;
-import android.os.IBinder;
-import android.util.Log;
-import android.widget.Toast;
-
-import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
-import androidx.core.app.NotificationCompat;
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class AlarmService extends BroadcastReceiver {
 
 
+    private CameraManager mCameraManager;
+    private String mCameraId;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     private void theActualJob(Context context) {
-        Log.i("MYTAG","ACTUAL JOB STARTED");
+        mCameraManager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
+        try {
+            mCameraId = mCameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
 
-        Toast.makeText(context, "Actual Job Started !", Toast.LENGTH_SHORT).show();
-        final Camera cam = Camera.open();
-        android.hardware.Camera.Parameters p = cam.getParameters();
-        p.setFlashMode(android.hardware.Camera.Parameters.FLASH_MODE_TORCH);
-        cam.setParameters(p);
-        cam.startPreview();
+        try {
+            mCameraManager.setTorchMode(mCameraId, true);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                cam.stopPreview();
-                cam.release();
+                try {
+                    mCameraManager.setTorchMode(mCameraId, false);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
             }
-        }, 30000);
+        },120000);
+
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onReceive(Context context, Intent intent) {
         theActualJob(context);
--- app/src/main/java/edu/sastra/a9oclock9minutes/MainActivity.java
+++ app/src/main/java/edu/sastra/a9oclock9minutes/MainActivity.java
@@ -5,19 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
 import android.app.AlarmManager;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.job.JobInfo;
-import android.app.job.JobScheduler;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-
 import java.util.Calendar;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,12 +22,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MyActivity", "Alarm On");
+
         AlarmManager am = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
         int ALARM_TYPE = AlarmManager.RTC_WAKEUP;
-
         Calendar calendar = Calendar.getInstance();
-        calendar.set(2020,4,3,12,17,0);
+        calendar.set(2020,Calendar.APRIL,3,13,45,0);
 
         Intent intent = new Intent(this, AlarmService.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(
@@ -42,9 +35,11 @@ public class MainActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             am.setExactAndAllowWhileIdle(ALARM_TYPE, calendar.getTimeInMillis(), pendingIntent);
         }
-        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             am.setExact(ALARM_TYPE, calendar.getTimeInMillis(), pendingIntent);
-        else
+        }
+        else {
             am.set(ALARM_TYPE, calendar.getTimeInMillis(), pendingIntent);
+        }
     }
 }
