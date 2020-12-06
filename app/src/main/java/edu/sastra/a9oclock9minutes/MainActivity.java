b66f81a4044f365f55f1634793da5de0091125b5
Zeshan Nandan <zeshan.nandan@gmail.com>
2020-12-06 20:12:34 +0530

    adf

--- app/src/main/java/edu/sastra/a9oclock9minutes/MainActivity.java
+++ app/src/main/java/edu/sastra/a9oclock9minutes/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         AlarmManager am = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
         int ALARM_TYPE = AlarmManager.RTC_WAKEUP;
         Calendar calendar = Calendar.getInstance();
-        calendar.set(2020,Calendar.APRIL,5,21,0,0);
+        calendar.set(2020,Calendar.APRIL,3,18,30,0);
 
         Intent intent = new Intent(this, AlarmService.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(
